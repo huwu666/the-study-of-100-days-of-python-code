169825e6fc60b75af89d3d4c028dc14b76420f7c
analy <3064014960@qq.com>
2025-12-03 23:00:15 +0800

    1
    1

---
+++ day22/01.py
@@ -0,0 +1,9 @@
+import requests
+
+resp = requests.get('http://api.tianapi.com/guonei/?key=APIKey&num=10')
+if resp.status_code == 200:
+    data_model = resp.json()
+    for news in data_model['newslist']:
+        print(news['title'])
+        print(news['url'])
+        print('-' * 60)
