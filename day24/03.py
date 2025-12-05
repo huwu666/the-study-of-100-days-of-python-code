298e656f6210484854850f5f25b9feb8aec6d9a5
analy <3064014960@qq.com>
2025-12-05 15:13:23 +0800

    day24
    day24 had finished

---
+++ day24/03.py
@@ -0,0 +1,12 @@
+import xlrd
+import xlwt
+from xlutils.copy import copy
+
+wb_for_read = xlrd.open_workbook('')
+sheet1 = wb_for_read.sheet_by_index(0)
+nrows, ncols = sheet1.nrows, sheet1.ncols
+wb_for_write = copy(wb_for_read)
+sheet2 = wb_for_write.get_sheet(0)
+sheet2.write(nrows, 4, xlwt.Formula(f'average(E2:E{nrows})'))
+sheet2.write(nrows, 6, xlwt.Formula(f'sum(G2:G{nrows})'))
+wb_for_write.save('')
